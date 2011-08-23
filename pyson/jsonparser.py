1f63f2fcb2c0200bf0d406bb10e37014c5f15073
Yogie Adrisatria <yogie.adrisatria@gmail.com>
2011-08-23 16:53:11 +0700

    add parser skeleton

--- pyson/jsonparser.py
+++ pyson/jsonparser.py
@@ -1,5 +1,5 @@
 import string
-
+from tokenizer import Tokenizer
 
 
 class JsonParser:
@@ -7,9 +7,22 @@ class JsonParser:
 		pass
 	
 	def parse(self,jsonString):
-		pass
+		tokenizer = Tokenizer()
+		self.tokens = tokenizer.tokenize()
+		self.idx = 0
 	
-	def lexer(self,jsonString):
+	def _start(self):
+		pass
+	def _getCurrentToken(self):
+		return self.tokens[self.idx]
+	
+	def _getNextToken(self):
 		pass
 		
-					
+	
+class ParseError(Exception):
+	def __init__(self,near=""):			
+		self.near = near
+	
+	def __str__(self):
+		return "Error near " + self.near 	
